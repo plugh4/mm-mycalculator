a02fb63db8845f68a7b020fb67e86eac99997d60
ravelasquez99 <ricky.wedew@gmail.com>
2016-03-17 12:13:23 -0700

    creating local repository

---
+++ myCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myCalculator
+//
+//  Created by Richard Velazquez on 3/17/16.
+//  Copyright © 2016 Richard Velazquez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
